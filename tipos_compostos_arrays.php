30912c5be8b8d9ad3d40275869571efc229bf3b7
CARLOS ANDRES FERRERO <anfer86@gmail.com>
2021-10-28 13:48:57 -0300

    Update tipos_compostos_arrays.php

--- tipos_compostos_arrays.php
+++ tipos_compostos_arrays.php
@@ -1,52 +1,41 @@
 <?php
 
-$arr_animais = array('gato','cachorro','pássaro');
-print_r($arr_animais);
+// Arrays simples
+$arr_simples = array('gato','cachorro','pássaro');
+print_r($arr_simples);
 echo '<br>';
 
-$arr_animais2 = ['gato', 'cachorro', 'pássaro'];
-print_r($arr_animais2);
+$arr_simples2 = ['gato', 'cachorro', 'pássaro'];
+print_r($arr_simples2);
 echo '<br>';
 
-$arr_animais3 = [ 
+// Array associativo (assoc)
+$arr_assoc = [ 
     'Tomero' => 'gato', 
     'Rutini' => 'gato',
     'Toby' => 'cachorro',
     'Chico' => 'pássaro'
 ];
-print_r($arr_animais3);
+print_r($arr_assoc);
 echo '<br>';
 
-echo 'O Tomero é um ' . $arr_animais3['Tomero'] . '<br>';
-echo 'O Chico é um ' . $arr_animais3['Chico'] . '<br>';
+echo 'As chaves ou keys são:   '. implode(', ', array_keys($arr_assoc)).'<br>';
+echo 'Os valores ou values são: '. implode(', ', array_values($arr_assoc)).'<br>';
 
-echo 'As keys são:   '. implode(', ', array_keys($arr_animais3)).'<br>';
-echo 'Os values são: '. implode(', ', array_values($arr_animais3)).'<br>';
+$nome = 'Tomero';
+echo "O $nome é um " . $arr_assoc[$nome] . '<br>';
 
-$keys = array_keys($arr_animais3);
-echo  $keys[3] . ' é ' . $arr_animais3[$keys[3]] . '<br>';
-echo  $keys[2] . ' é ' . $arr_animais3[$keys[2]] . '<br>';
+$keys = array_keys($arr_assoc);
+echo  $keys[3] . ' é ' . $arr_assoc[$keys[3]] . '<br>';
 
-
-if (in_array('Tomero', array_keys($arr_animais3))){
-    echo 'Tomero está no array de animais <br>';
-}
-
-if (in_array('Duda', array_keys($arr_animais3))){
-    echo 'Duda está no array de animais <br>';
+if (in_array($nome, array_keys($arr_assoc))){
+    echo "$nome está no array de animais <br>";
 } else {
-    echo 'Duda não está no array de animais <br>';
+    echo "$nome não está no array de animais <br>";
 }
 
-$arr_animais3['Duda'] = 'pássaro';
-if (in_array('Duda', array_keys($arr_animais3))){
-    echo 'Duda está no array de animais <br>';
-} else {
-    echo 'Duda não está no array de animais <br>';
-}
-
-unset($arr_animais3);
-echo 'Apagamos a variável erray $arr_animais3 <br>';
-print_r($arr_animais3);
+unset($arr_assoc);
+echo 'Apagamos a variável $arr_assoc <br>';
+print_r($arr_assoc);
 
 ?>
